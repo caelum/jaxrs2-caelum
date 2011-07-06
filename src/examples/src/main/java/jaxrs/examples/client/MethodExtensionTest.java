9cd26f59d74be356b6438b533a21755c11a94aa1
Guilherme Silveira <guilherme.silveira@caelum.com.br>
2011-07-06 12:55:09 -0300

    showing how to do typesafe entity checking

--- src/examples/src/main/java/jaxrs/examples/client/MethodExtensionTest.java
+++ src/examples/src/main/java/jaxrs/examples/client/MethodExtensionTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientFactory;
+import javax.ws.rs.client.DelegateInvocation;
+import javax.ws.rs.client.ExtensionFactory;
+import javax.ws.rs.client.Invocation;
 import javax.ws.rs.core.HttpResponse;
 
 import org.junit.Test;
@@ -25,5 +28,44 @@ public class MethodExtensionTest {
         HttpResponse response = client.request("http://www.google.com/").with(webdav()).propfind();
         assertEquals(200, response.getStatusCode());
 	}
+
+
+	class TypeSafedWithBody extends DelegateInvocation{
+		private Object entity;
+		public TypeSafedWithBody(Invocation wrapper, Object entity) {
+			super(wrapper);
+			this.entity = entity;
+		}
+		public HttpResponse postChecked() {
+			// do everything
+			return wrapped.invoke();
+		}
+	}
+
+	class TypeSafeEntity implements ExtensionFactory<TypeSafedWithBody>{
+		private final Object entity;
+
+		public TypeSafeEntity(Object entity) {
+			this.entity = entity;
+		}
+
+		public TypeSafedWithBody wrap(Invocation invocation) {
+			return new TypeSafedWithBody(invocation, entity);
+		}
+	}
+
+	public TypeSafeEntity entity(Object entity) {
+		return new TypeSafeEntity(entity); 
+	}
+
+	@Test
+	public void shouldBeCapableOfCompileTimeCheckingMissingMethods() {
+		String myObject = "something that will be posted. any object as usual.";
+		
+        Client client = ClientFactory.newClient();
+        HttpResponse response = client.request("http://www.google.com/").with(entity(myObject)).postChecked();
+        assertEquals(200, response.getStatusCode());
+	}
+
 
 }
